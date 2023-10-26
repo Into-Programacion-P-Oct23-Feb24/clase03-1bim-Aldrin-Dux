dd374a6f92d35aa3dbaa6e464629e72936fa7fe1
unknown <apadilla2@utpl.edu.ec>
2023-10-26 16:31:17 -0500

    ejercicio en clase 26/10/2023

---
+++ ejemplo02/src/ejemplo02/Ejemplo02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo02;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
